18ef9d3f1262eda769254f67de40a098dc5c5a16
Nathan Smith <nathan@smithfam.info>
2011-05-28 18:42:17 -0700

    I really should commit more often. And with better commit messages.

--- spigot.py
+++ spigot.py
@@ -18,26 +18,22 @@
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 import ConfigParser
+from datetime import datetime
+import hashlib
 import logging
 import os
 import sqlite3
 import sys
+from time import mktime
 
-#try:
- #   import statusnet
-#except ImportError, e:
-#    print "Error: %s" % e
-#    sys.exit(2)
+import feedparser
 
-try:
-    import feedparser
-except ImportError, e:
-    print "Error: %s" % e
-    sys.exit(2)
+# import statusnet
+
 
 class SpigotDB():
     """Handle database calls for Spigot"""
-    
+ 
     def __init__(self, path="spigot.db"):
         self.path = path
         self._connect()
@@ -48,9 +44,10 @@ class SpigotDB():
         """Establish the database connection for this instantiation."""
 
         # Check first for a database file
+        new_db = False
         if not os.path.exists(self.path):
             new_db = True
-            logging.info("Database file '%s' does not exist" % self.path)
+            logging.info("Database file %s does not exist" % self.path)
         try:
             self._db = sqlite3.connect(self.path)
         except:
@@ -66,7 +63,7 @@ class SpigotDB():
         curs = self._db.cursor()
         # Figure out db tables based on tricklepost
         create_query = """create table items (feed text, link text, title text,
-            hash text, account text, published text)"""
+            hash text, account text, date timestamp, posted timestamp)"""
         curs.execute(create_query)
         self._db.commit()
         logging.info("Initialized database tables")
@@ -79,6 +76,30 @@ class SpigotDB():
         
         self._db.close()
         logging.debug("Closed connection to database")
+        
+    def check_hash(self, item_hash):
+        """Returns true if the specified hash is already in the database."""
+        
+        curs = self._db.cursor()
+        curs.execute("select * from items where hash=?", [item_hash])
+        if len(curs.fetchall()) > 0:
+            return True
+        else:
+            return False
+        curs.close()
+            
+    def add_item(self, feed_url, link, title, item_hash, account, date):
+        """Add an item to the database with the given parameters. Return True if
+        successful."""
+        
+        curs = self._db.cursor()
+        curs.execute("insert into items(feed, link, title, hash, account, \
+            date) values (?, ?, ?, ?, ?, ?)",
+            (feed_url, link, title, item_hash, account, date))
+        logging.debug("    Added item %s to database" % item_hash)
+        curs.close()
+        self._db.commit()
+        
 
 class SpigotFeedPoller():
     """
@@ -88,18 +109,19 @@ class SpigotFeedPoller():
 
     """
 
-    def __init__(self):
+    def __init__(self, db):
+        self._spigotdb = db
         self.feeds_to_poll = []     
         self.feeds_to_poll = self.parse_config()
-        for feed in self.feeds_to_poll:
-            self.scan_feed(feed)
-        
+        for feed_url, account in self.feeds_to_poll:
+            self.scan_feed(feed_url, account)
+
     def parse_config(self):
         """Returns a list of syndicated feeds to check for new posts."""
         
         # Make feeds to poll an internal variable for this function
         feeds_to_poll = []
-        logging.info("Loading feeds.conf")
+        logging.debug("Loading feeds.conf")
         feeds_config = ConfigParser.RawConfigParser()
         if not feeds_config.read("feeds.conf"):
             logging.error("Could not parse feeds.conf")
@@ -111,29 +133,71 @@ class SpigotFeedPoller():
         else:
             logging.info("Found %d feeds in feeds.conf" % feeds_num)
         for feed in feeds:
-            logging.debug("Processing feed '%s'" % feed)
+            logging.debug("Processing feed %s" % feed)
+            
             # Ensure that the feed section has the needed attributes
             # If not, treat as a non-fatal error, but warn the user
             if ( feeds_config.has_option(feed, "url") &
                      feeds_config.has_option(feed, "account") &
                      feeds_config.has_option(feed, "interval") ):
-                logging.debug("  URL: %s" % feeds_config.get(feed, "url"))
-                logging.debug("  Account: %s" % feeds_config.get(feed,
-                    "account"))
+                url = feeds_config.get(feed, "url")
+                logging.debug("  URL: %s" % url)
+                account = feeds_config.get(feed, "account")
+                logging.debug("  Account: %s" % account)
                 logging.debug("  Interval: %s min" % feeds_config.get(feed,
                     "interval"))
-                feeds_to_poll.append(feed)
+                feeds_to_poll.append((url, account))
                 logging.debug("  Added to list of feeds to poll")
                 
             else:
                 logging.warning("  Missing necessary options, skipping")
         return feeds_to_poll
+
+    def scan_feed(self, feed_url, account):
+        """Poll the given feed and then update the database with new info"""
+
+        logging.debug("Polling feed %s for new items" % feed_url)
+        # Allow for parsing of this feed to fail without raising an exception
         
-        def scan_feed(self, feed):
-            """Poll the given feed and then update the database with new info"""
-            
-            p
-            pass
+        try:
+            p = feedparser.parse(feed_url)
+        except:
+            logging.error("Unable to parse feed %s" % feed_url)
+            return None
+        # Get a list of items for the feed and compare it to the database
+        num_items = len(p.entries)
+        logging.debug("Found %d items in feed %s" % (num_items,feed_url))
+        # Find out which encoding the feed uses to avoid problems with hashlib
+        # below
+        enc = p.encoding
+        new_items = 0
+        for i in range(len(p.entries)):
+            logging.debug("  Processing item %d" % i)
+            title = p.entries[i].title
+            logging.debug("    Title: %s" % title)
+            link = p.entries[i].link
+            logging.debug("    Link: %s" % link)
+            date = p.entries[i].date_parsed
+            date_struct = datetime.fromtimestamp(mktime(date))
+            logging.debug("    Date: %s" % datetime.isoformat(date_struct))
+            # Create a md5 hash of title and link, so that we can
+            # easily tell duplicates in the feed. Could use guid for this, but
+            # I don't trust feeds to use it correctly.
+            h = hashlib.md5()
+            hash_input = "%s|%s" % (title, link)
+            h.update(hash_input.encode(enc))
+            item_hash = h.hexdigest()
+            logging.debug("    Hash: %s" % item_hash)
+            # Check to see if item has already entered the database
+            if not self._spigotdb.check_hash(item_hash):
+                logging.debug("    Not in database")
+                self._spigotdb.add_item(feed_url, link, title, item_hash, account,
+                    date_struct)
+                new_items += 1
+            else:
+                logging.debug("    Already in database")
+        logging.info("Found %d new items in feed %s" % (new_items, feed_url))
+                
 
 
 if __name__ == "__main__":
@@ -149,7 +213,7 @@ if __name__ == "__main__":
 #        logging.error("Could not parse accounts.conf")
 #        sys.exit(2)
     spigot_db = SpigotDB()
-    spigot_feed = SpigotFeedPoller()
+    spigot_feed = SpigotFeedPoller(spigot_db)
     
     
 # TODO
